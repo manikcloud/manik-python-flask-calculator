0461b6366364589e2edc2b14c8191399eb4b4672
varun <varun@gmail.com>
2023-04-19 23:01:53 +0800

    python new

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, os
 
 app = Flask(__name__)
 
@@ -41,5 +41,7 @@ def calculate():
     return render_template('result.html', result=result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 
