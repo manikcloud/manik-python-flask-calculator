491faf350ae52cd30e412212db1eb7aaba5e060d
varun <varun@gmail.com>
2023-04-19 17:25:05 +0800

    python

--- app.py
+++ app.py
@@ -1,52 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        num1 = float(request.form['num1'])
+        num2 = float(request.form['num2'])
+        operator = request.form['operator']
+
+        if operator == 'add':
+            result = num1 + num2
+        elif operator == 'subtract':
+            result = num1 - num2
+        elif operator == 'multiply':
+            result = num1 * num2
+        elif operator == 'divide':
+            if num2 == 0:
+                return "Error: Cannot divide by zero"
+            result = num1 / num2
+        else:
+            return "Error: Invalid operator"
+
+        return redirect(url_for('result', result=result))
+
     return render_template('index.html')
 
-@app.route('/add', methods=['POST'])
-def add():
-    try:
-        num1 = float(request.form['num1'])
-        num2 = float(request.form['num2'])
-        result = num1 + num2
-        return str(result)
-    except ValueError:
-        return "Error: Invalid input"
-
-@app.route('/subtract', methods=['POST'])
-def subtract():
-    try:
-        num1 = float(request.form['num1'])
-        num2 = float(request.form['num2'])
-        result = num1 - num2
-        return str(result)
-    except ValueError:
-        return "Error: Invalid input"
-
-@app.route('/multiply', methods=['POST'])
-def multiply():
-    try:
-        num1 = float(request.form['num1'])
-        num2 = float(request.form['num2'])
-        result = num1 * num2
-        return str(result)
-    except ValueError:
-        return "Error: Invalid input"
-
-@app.route('/divide', methods=['POST'])
-def divide():
-    try:
-        num1 = float(request.form['num1'])
-        num2 = float(request.form['num2'])
-        if num2 == 0:
-            return "Error: Cannot divide by zero"
-        result = num1 / num2
-        return str(result)
-    except ValueError:
-        return "Error: Invalid input"
+@app.route('/result/<float:result>')
+def result(result):
+    return render_template('result.html', result=result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
