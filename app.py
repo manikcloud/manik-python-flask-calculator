71dd8b6d90b531d0aa5c19a315cc4cd46330dce7
varun <varun@gmail.com>
2023-04-21 13:08:11 +0800

    smoke-test health

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
 
+# create a Flask application instance
 app = Flask(__name__)
 
+# define the index route
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# define the result route
 @app.route('/result')
 def result():
     return render_template('result.html')
 
+# define the error route
 @app.route('/error')
 def error():
     return render_template('error.html')
 
+# define the calculate route
 @app.route('/calculate', methods=['POST'])
 @app.route('/calculate', methods=['POST'])
 def calculate():
@@ -41,10 +46,12 @@ def calculate():
 
     return render_template('result.html', result=result)
 
+# define the health check route
 @app.route('/health')
 def health():
     return 'OK'
 
+# run the application
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5000)
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
